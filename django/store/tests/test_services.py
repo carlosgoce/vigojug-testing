b8b283dd73cb4a5b0fa8b1533f49daffb35adcf5
Carlos Goce <carlosgoce@gmail.com>
2018-03-03 13:13:45 +0100

    Add mark

--- django/store/tests/test_services.py
+++ django/store/tests/test_services.py
@@ -1,6 +1,9 @@
+import pytest
+
 from store import services
 
 
+@pytest.mark.slow
 def test_coordinates_for_address():
     coordinates = services.coordinates_for_address('Kaleido Coworking')
     assert {'lat': 42.2374301, 'lng': -8.717658} == coordinates
