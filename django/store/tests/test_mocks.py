d94c466e2874509b04f750c897e93d09a4eccab7
Carlos Goce <carlosgoce@gmail.com>
2018-03-04 00:59:05 +0100

    Improve test

--- django/store/tests/test_mocks.py
+++ django/store/tests/test_mocks.py
@@ -10,12 +10,11 @@ from store.models import Seller
 @pytest.mark.django_db
 def test_set_location_when_location_is_empty():
     seller = Seller(name='Carlos', address='Rúa Roupeiro', city='Vigo')
-    seller.save()
-
-    assert seller.lat == 12345
-    assert seller.lng == 54321
+    seller.set_location()
+    assert (12345, 54321) == (seller.lat, seller.lng)
 
 
 # TODO
+# Añadir ejemplo con PHP y Injección de dependencias
 # Añadir otro test the location ya definida
 # Hacer otro más de python parametrize
